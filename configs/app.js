d686e2493c5abab3b2161a8e67fe6b4f44136a92
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-04-28 08:10:41 -0600

    se crea archivo app.js

---
+++ configs/app.js
@@ -0,0 +1,37 @@
+'use strict'
+
+import express from 'express' 
+import morgan from 'morgan' 
+import helmet from 'helmet' 
+import cors from 'cors' 
+
+
+import passRoutes from '../src/Ejercicio4/pass.routes.js'
+
+
+const configs = (app)=>{
+    app.use(express.json())
+    app.use(express.urlencoded({extended: true}))
+    app.use(cors())
+    app.use(helmet())
+    app.use(morgan('dev'))
+}
+
+const routes = (app)=>{
+    app.use('/v1/randomPass', passRoutes)
+   
+}
+
+export const initServer = async()=> {
+    const app = express()
+
+    try{
+        configs(app)
+        routes(app)
+        app.listen(process.env.PORT)
+        console.log(`Server running in port ${process.env.PORT}`)
+    }catch(err){
+        console.log('Server init failed', err)
+    }
+    
+}
